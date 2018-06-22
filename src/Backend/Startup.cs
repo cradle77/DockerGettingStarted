2b6f9b5a40e0d3ba143a60e4183c0292e77bc36a
marcodes <marcodesanctis2@gmail.com>
2018-06-22 23:12:29 +0100

    dotnet core2.1

--- src/Backend/Startup.cs
+++ src/Backend/Startup.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
@@ -22,7 +19,8 @@ namespace Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSingleton<IMongoClient>(x => new MongoClient(Configuration.GetConnectionString("mongo")));
             services.AddTransient<IMongoDatabase>(x => x.GetService<IMongoClient>().GetDatabase("People"));
@@ -34,7 +32,6 @@ namespace Backend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
--- src/Frontend/Startup.cs
+++ src/Frontend/Startup.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Frontend.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Frontend.Data;
-using Microsoft.Extensions.Caching.Distributed;
 using StackExchange.Redis;
-using Microsoft.AspNetCore.DataProtection;
 
 namespace Frontend
 {
@@ -25,13 +22,10 @@ namespace Frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            var redis = ConnectionMultiplexer.Connect(Configuration.GetSection("cache").GetValue<string>("redis"));
-
-            services.AddDataProtection()
-                .SetApplicationName("frontend-test")
-                .PersistKeysToRedis(redis);
+            setupDataProtection(services);
 
             services.AddDistributedRedisCache(options =>
             {
@@ -44,13 +38,24 @@ namespace Frontend
                 x.GetService<IDistributedCache>()));
         }
 
+        private void setupDataProtection(IServiceCollection services)
+        {
+            if (!Configuration.GetValue<bool>("isInCluster"))
+                return;
+
+            var redis = ConnectionMultiplexer.Connect(Configuration.GetSection("cache").GetValue<string>("redis"));
+
+            services.AddDataProtection()
+                .SetApplicationName("frontend-test")
+                .PersistKeysToRedis(redis);
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
